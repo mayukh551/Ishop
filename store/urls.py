806b4e10fdb0bac0bc456d6332fbf9507ecba855
Rounak Hazra <rounakhazra999@gmail.com>
2022-04-11 12:44:01 +0530

    update I 11/4/22 validation of email done -- code refactored in views.py

--- store/urls.py
+++ store/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from .views import index, signup
 urlpatterns = [
-    path('', index),
+    path('', index, name='homepage'),
     path('signup', signup)
 
 ]
--- store/views.py
+++ store/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.hashers import make_password, check_password
 from .models.category import Category
 from .models.product import Product
 from .models.customer import Customer
@@ -20,67 +21,80 @@ def index(request):
     data['categories'] = categories
     return render(request, 'index.html', data)
 
+def validateCustomer(customer):
+    # form validation
+    pswrd = str(customer.password)
+    uc = False
+    lc = False
+    n = False
+    for i in pswrd:
+        if i.isupper():
+            uc = True
+        elif i.islower():
+            lc = True
+        elif i.isnumeric():
+            n = True
+
+    error_msg = None
+    if not customer.first_name:
+        error_msg = "First name required !"
+    elif not customer.last_name:
+        error_msg = "Last name required !"
+    elif not customer.phone:
+        error_msg = "Phone number required !"
+    elif len(customer.password) < 8:
+        error_msg = "Password must be atleast 8 characters long !"
+    elif uc == False or lc == False or n == False:
+        error_msg = "Password must contain atleast 1 upper-case alphabet, 1 lower-case alphabet and 1 number"
+    elif not (customer.password == customer.repassword):
+        error_msg = "Re-entered password is not same"
+    elif customer.isExists():
+        error_msg = "A user-account already exists with this email"
+
+    return error_msg
+
+def registerUser(request):
+    postData = request.POST
+    first_name = postData.get('firstname')
+    last_name = postData.get('lastname')
+    phone = postData.get('phone')
+    email = postData.get('email')
+    password = postData.get('password')
+    repassword = postData.get('repassword')
+    error_msg = None
+
+    customer = Customer(first_name=first_name,
+                        last_name=last_name,
+                        phone=phone,
+                        email=email,
+                        password=password,
+                        repassword=repassword)
+    error_msg = validateCustomer(customer)
+
+    value = {
+        'first_name': first_name,
+        'last_name': last_name,
+        'phone': phone,
+        'email': email
+    }
+
+    # registration of the customer (saving profile)
+    if not error_msg:
+        customer.password = make_password(customer.password)
+        customer.register()
+        return redirect('homepage')
+        # return HttpResponse("sign up success")
+    else:
+        data = {
+            'error': error_msg,
+            'values': value
+        }
+        return render(request, 'signup.html', data)
 
 def signup(request):
     if request.method == 'GET':
         return render(request, 'signup.html')
     else:
-        postData = request.POST
-        first_name = postData.get('firstname')
-        last_name = postData.get('lastname')
-        phone = postData.get('phone')
-        email = postData.get('email')
-        password = postData.get('password')
-        repassword = postData.get('repassword')
+        return registerUser(request)
 
-        # form validation
-        pswrd = str(password)
-        uc = False
-        lc = False
-        n = False
-        for i in pswrd:
-            if i.isupper():
-                uc = True
-            elif i.islower():
-                lc = True
-            elif i.isnumeric():
-                n = True
 
-        error_msg = None
-        if not first_name:
-            error_msg = "First name required !"
-        elif not last_name:
-            error_msg = "Last name required !"
-        elif not phone:
-            error_msg = "Phone number required !"
-        elif len(password) < 8:
-            error_msg = "Password must be atleast 8 characters long !"
-        elif uc == False or lc == False or n == False:
-            error_msg = "Password must contain atleast 1 upper-case alphabet, 1 lower-case alphabet and 1 number"
-        elif not (password == repassword):
-            error_msg = "Re-entered password is not same"
-
-        value = {
-                 'first_name' : first_name,
-                 'last_name' : last_name,
-                 'phone' : phone,
-                 'email' : email
-                }
-
-        # registration of the customer (saving profile)
-        if not error_msg:
-            customer = Customer(first_name=first_name,
-                                last_name=last_name,
-                                phone=phone,
-                                email=email,
-                                password=password,
-                                repassword=repassword)
-            customer.register()
-
-            return HttpResponse("sign up success")
-        else:
-            data={
-                'error':error_msg,
-                'values':value
-            }
-            return render(request, 'signup.html', data)
